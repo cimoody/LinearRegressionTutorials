0c8f26579008723ccc436b5d81fb9785f8e44c30
cimoody <cristina.isadora.moody@gmail.com>
2015-08-05 16:37:14 -0400

    Initial Commit from Coursera on Regression.

---
+++ Coursera_Regression.R
@@ -0,0 +1,10 @@
+# Coursera_Regression.R
+# code by Cristina Moody
+# following https://www.youtube.com/watch?v=1akVPR0LDsg&list=PLpl-gQkQivXjqHAJd2t-J_One_fYE55tC
+
+# Install Packages
+install.packages("UsingR");
+
+
+# Loading packages
+library(UsingR);
